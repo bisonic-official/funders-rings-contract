4ea575e51f42a5a0b357df61ab1e376d3f88b4f6
Rodolfo Ferro <rodolfoferroperez@gmail.com>
2023-10-16 18:35:08 -0600

    Adds contract scripts for setup.

---
+++ contract_test_mint.py
@@ -0,0 +1,53 @@
+from utils.config import load_config
+from utils.config import setup_custom_logger
+from utils.contract import connect_to_web3
+from utils.contract import load_contract
+from utils.transact import mint_rings
+
+
+def main():
+    """The main function to mint and NFT."""
+
+    # Load config and setup logger
+    config = load_config('config.ini')
+    logger = setup_custom_logger()
+
+    # Connect to web3
+    w3, status = connect_to_web3(network=config['network']['network'],
+                                 api_key=config['network']['api_key'])
+    private_key = config['account']['private_key']
+    address = config['account']['address']
+
+    if status:
+        connection_msg = 'Web3 connection successful!'
+        print(f'[INFO] {connection_msg}')
+        logger.info(connection_msg)
+
+        # Load the contract
+        contract = load_contract(w3, config['contract']['address'],
+                                 config['contract']['abi'])
+
+        # Get vault address
+        vault_address = contract.functions.vault().call()
+        print(f'[INFO] Vault address: {vault_address}')
+
+        # Get rings available before mint
+        rings_available = contract.functions.getAvailableRings().call()
+        print(f'[INFO] Rings available: {rings_available}')
+
+        # Verify the ring price before mint
+        ring_price = contract.functions.getRingPrice().call()
+        print(f'[INFO] Ring price: {ring_price}')
+
+        # Mint a ring
+        txn_receipt = mint_rings(w3, contract, private_key, address, 1,
+                                 ring_price)
+        print(f'[INFO] Transaction receipt: {txn_receipt}')
+
+        # Get rings available after mint
+        rings_available = contract.functions.getAvailableRings().call()
+        print(f'[INFO] Rings available: {rings_available}')
+
+
+if __name__ == '__main__':
+    main()
--- utils/transact.py
+++ utils/transact.py
@@ -141,3 +141,91 @@ def mint_rings(w3, contract, private_key, owner_address, num_rings,
     logger.info(log_msg)
 
     return txn_receipt
+
+
+def withdraw_eth(w3, contract, private_key, owner_address, amount):
+    """Withdraw ETH.
+    
+    Parameters
+    ----------
+    w3 : Web3
+        The web3 object.
+    contract
+        The contract object.
+    private_key : str
+        The private key.
+    owner_address : str
+        The owner address.
+    amount : int
+        The amount of Mana to withdraw.
+    
+    Returns
+    -------
+    txn : dict
+        The transaction dictionary.
+    """
+
+    logger = logging.getLogger('buyer')
+
+    txn = contract.functions.withdraw(amount).build_transaction({
+        'nonce':
+        w3.eth.get_transaction_count(owner_address),
+        'gas':
+        100000
+    })
+
+    # Sign the transaction
+    txn_signed = w3.eth.account.sign_transaction(txn, private_key)
+
+    # Send the transaction and wait for the transaction receipt
+    txn_hash = w3.eth.send_raw_transaction(txn_signed.rawTransaction)
+    txn_receipt = w3.eth.wait_for_transaction_receipt(txn_hash)
+    txn_receipt = txn_receipt.transactionHash.hex()
+
+    log_msg = f"TXN with hash: { txn_receipt }"
+    logger.info(log_msg)
+
+    return txn_receipt
+
+
+def withdraw_all_eth(w3, contract, private_key, owner_address):
+    """Withdraw all Mana.
+    
+    Parameters
+    ----------
+    w3 : Web3
+        The web3 object.
+    contract
+        The contract object.
+    private_key : str
+        The private key.
+    owner_address : str
+        The owner address.
+    
+    Returns
+    -------
+    txn : dict
+        The transaction dictionary.
+    """
+
+    logger = logging.getLogger('buyer')
+
+    txn = contract.functions.withdrawAll().build_transaction({
+        'nonce':
+        w3.eth.get_transaction_count(owner_address),
+        'gas':
+        100000
+    })
+
+    # Sign the transaction
+    txn_signed = w3.eth.account.sign_transaction(txn, private_key)
+
+    # Send the transaction and wait for the transaction receipt
+    txn_hash = w3.eth.send_raw_transaction(txn_signed.rawTransaction)
+    txn_receipt = w3.eth.wait_for_transaction_receipt(txn_hash)
+    txn_receipt = txn_receipt.transactionHash.hex()
+
+    log_msg = f"TXN with hash: { txn_receipt }"
+    logger.info(log_msg)
+
+    return txn_receipt
